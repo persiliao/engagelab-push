378b23ce70b40ab6458816630141df0a938e245b
persiliao <xiangchu.liao@gmail.com>
2023-05-23 02:04:39 +0800

    refactor: Code optimization

--- src/Push.php
+++ src/Push.php
@@ -12,11 +12,6 @@ class Push extends Service {
     public static function send(array $body) {
         return self::getHttpClient()->post(self::$service, [
             'body' => json_encode($body),
-            'headers' => [
-                'Content-Type' => 'application/json',
-                'Accept' => 'application/json',
-                'Authorization' => sprintf("Basic %s", App::getInstance()->getAuth()),
-            ],
         ]);
     }
 }
--- src/Service.php
+++ src/Service.php
@@ -9,6 +9,11 @@ abstract class Service {
     protected static function getHttpClient(): Client {
         return new Client([
             'base_uri' => App::getInstance()->getDomain(),
+            'headers' => [
+                'Content-Type' => 'application/json',
+                'Accept' => 'application/json',
+                'Authorization' => sprintf("Basic %s", App::getInstance()->getAuth()),
+            ],
         ]);
     }
 }
