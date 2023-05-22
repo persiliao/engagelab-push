af55652d0c62e9d405379636f62e80bec239fea4
persiliao <xiangchu.liao@gmail.com>
2023-05-22 23:31:26 +0800

    refactor: Code optimization

--- src/Service.php
+++ src/Service.php
@@ -8,19 +8,11 @@ abstract class Service {
 
     protected static function getHttpClient(): Client {
         return new Client([
-            'baseUrl' => static::getApiUrl(),
+            'base_uri' => App::getInstance()->getDomain(),
             'headers' => [
                 'Accept' => 'application/json',
                 'Authorization' => sprintf("Basic %s", App::getInstance()->getAuth()),
             ],
         ]);
-    }
-
-    protected static function buildMessage() {
-
-    }
-
-    protected static function getApiUrl(): string {
-        return App::getInstance()->buildServiceApiUrl(static::$service);
     }
 }
