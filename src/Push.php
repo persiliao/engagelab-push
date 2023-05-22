5ac5ae376f224810650303083627c64a4c911d96
persiliao <xiangchu.liao@gmail.com>
2023-05-23 05:08:46 +0800

    refactor: Code optimization

--- src/Push.php
+++ src/Push.php
@@ -14,4 +14,130 @@ class Push extends Service {
             'body' => json_encode($body),
         ]);
     }
+
+    /**
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public static function sendRegistrationNotification(
+        array $registrationIds,
+              $alert,
+              $title = '',
+              $platform = 'all',
+              $androidOptions = [],
+              $iosOptions = [],
+              $options = []
+    ) {
+        $to = [
+            'registration_id' => $registrationIds,
+        ];
+
+        return self::sendNotification($to, $alert, $title, $platform, $androidOptions, $iosOptions, $options);
+    }
+
+    /**
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public static function sendAllNotification(
+        $alert,
+        $title = '',
+        $platform = 'all',
+        $androidOptions = [],
+        $iosOptions = [],
+        $options = []
+    ) {
+        return self::sendNotification('all', $alert, $title, $platform, $androidOptions, $iosOptions, $options);
+    }
+
+    /**
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public static function sendNotification(
+        $to,
+        $alert,
+        $title = '',
+        $platform = 'all',
+        $androidOptions = [],
+        $iosOptions = [],
+        $options = []
+    ) {
+        $body = [
+            'to' => $to,
+            'body' => [
+                'platform' => $platform,
+                'notification' => [
+                    'android' => array_merge([
+                        'alert' => $alert,
+                        "title" => $title,
+                    ], $androidOptions),
+                    'ios' => array_merge([
+                        "alert" => $alert,
+                    ], $iosOptions),
+                ],
+            ],
+        ];
+
+        return self::send(array_merge($body, $options));
+    }
+
+    /**
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public static function sendMessage(
+        $to,
+        $message,
+        $title = '',
+        $contextType = 'text',
+        $platform = 'all',
+        $extras = [],
+        $options = []
+    ) {
+        $body = [
+            'to' => $to,
+            'body' => [
+                'platform' => $platform,
+                'message' => [
+                    'msg_content' => $message,
+                    'content_type' => $contextType,
+                    'title' => $title,
+                    'extras' => $extras,
+                ],
+            ],
+        ];
+
+        return self::send(array_merge($body, $options));
+    }
+
+    /**
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public static function sendRegistrationMessage(
+        array $registrationIds,
+              $message,
+              $title = '',
+              $contextType = 'text',
+              $platform = 'all',
+              $extras = [],
+              $options = []
+    ) {
+        $to = [
+            'registration_id' => $registrationIds,
+        ];
+
+        return self::sendMessage($to, $message, $title, $contextType, $platform, $extras, $options);
+    }
+
+    /**
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public static function sendAllMessage(
+
+        $message,
+        $title = '',
+        $contextType = 'text',
+        $platform = 'all',
+        $extras = [],
+        $options = []
+    ) {
+        return self::sendMessage('all', $message, $title, $contextType, $platform, $extras, $options);
+    }
 }
